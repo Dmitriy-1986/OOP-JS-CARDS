0e1f351201d476d390d1f6f3f3a4f66c3da8a27d
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2022-09-24 13:37:53 +0300

    Add files via upload

---
+++ obj-cards.js
@@ -0,0 +1,17 @@
+const card0 = new Card('Notebook', 'Apple', 'Best apple product', '10000');
+card0.viewCard(); 
+
+const card1 = new Card('Notebook', 'Samsung', 'Best Samsung product', '1000');
+card1.viewCard();
+
+const card2 = new Card('Notebook', 'Asus', 'Best Asus product', '1000');
+card2.viewCard();
+
+const card3 = new Card('Notebook', 'Aser', 'Best Aser product', '7000');
+card3.viewCard();
+
+const card4 = new Card('Notebook', 'HP', 'Best Hp product', '6000');
+card0.viewCard();
+
+const card5 = new Card('Notebook', 'Dell', 'Best Dell product', '5000');
+card5.viewCard();
